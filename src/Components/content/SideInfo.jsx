f0a23c25b56424285f01a146965ed27678538c59
karam-zomlut <zomlutk@gmail.com>
2022-10-25 23:20:18 +0300

    Add Compititions in the SideInfo

--- src/Components/content/SideInfo.jsx
+++ src/Components/content/SideInfo.jsx
@@ -1,51 +1,75 @@
-import { Box, List, Typography, useMediaQuery } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import Lego from "../../assets/images/lego.png";
+import { Box, List, Typography, useMediaQuery } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getCompetitions } from '../../Services';
 
 const SideInfo = () => {
-  const matches = useMediaQuery("(max-width:1000px)");
+  const matches = useMediaQuery('(max-width:1000px)');
   const [value, setValue] = useState();
 
+  const [competitions, setCompetitions] = useState([]);
+
+  const fetchCompititions = async () => {
+    const response = await getCompetitions('');
+    const europComp = await getCompetitions('id=401');
+    const champComp = await getCompetitions('id=400');
+    console.log(europComp?.data?.data[0]);
+    console.log(champComp?.data?.data[0]);
+    setCompetitions([
+      ...response.data.data.slice(0, 5),
+      europComp?.data?.data[0],
+      champComp?.data?.data[0],
+    ]);
+  };
+
   useEffect(() => {
-    if (matches) setValue("none");
+    fetchCompititions();
+  }, []);
+
+  useEffect(() => {
+    if (matches) setValue('none');
   }, [matches]);
 
   return (
-    <Box display={value} style={{minWidth: '17%'}}>
+    <Box display={value} style={{ minWidth: '17%' }}>
       <List
-        sx={{ background: "#F8F8F8", padding: "2px 2px" }}
-        direction="column">
-        {[0, 1, 2, 3, 4, 5].map((el) => (
+        sx={{ background: '#F8F8F8', padding: '2px 2px' }}
+        direction='column'
+      >
+        {competitions.map((compitition) => (
           <li
-            key={el}
+            key={compitition?.id}
             style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "start",
-              padding: "8px 5px",
-              borderBottom: "1px solid rgb(225 225 225)",
-            }}>
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'start',
+              padding: '8px 5px',
+              borderBottom: '1px solid rgb(225 225 225)',
+            }}
+          >
             <img
-              src={Lego}
-              alt=""
-              width="28px"
-              height="28px"
-              style={{ marginLeft: "10px" }}
+              src={`https://cdn.so3ody.com/scores/competitions/100x130/${compitition?.id}.png`}
+              alt=''
+              width='28px'
+              height='28px'
+              style={{ marginLeft: '10px' }}
             />
-            <Typography variant="h6" style={{ fontSize: "15px" }}>
-              الدوري الإنجليزي لكرة القدم
-            </Typography>
+            <Link to={`/league/${compitition?.id}`} style={{color: '#000', textDecoration: 'none'}}>
+              <Typography variant='h6' style={{ fontSize: '15px' }}>
+                {compitition?.name}
+              </Typography>
+            </Link>
           </li>
         ))}
       </List>
       <img
         style={{
-          marginTop: "25px",
-          border: "3px solid #94b7db",
-          objectFit: "contain",
+          marginTop: '25px',
+          border: '3px solid #94b7db',
+          objectFit: 'contain',
         }}
-        src="https://via.placeholder.com/200X600/#DFE6FA"
-        alt=""
+        src='https://via.placeholder.com/200X600/#DFE6FA'
+        alt=''
       />
     </Box>
   );
