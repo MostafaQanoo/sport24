8fb0b1fc9e30f6ba6c0c08f4cbf4fddc7b8203c9
MohammadAlHabil <mohammadalhabil1999@gmail.com>
2022-10-26 02:08:06 +0300

    edit the image of competition

--- src/Pages/Leagues.jsx
+++ src/Pages/Leagues.jsx
@@ -1,39 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import { ArrangeTable, MatchesTable, PageBanner, RangeTeamsGoals, TeamStatistics } from '../Components';
-import PremierLeague from '../assets/images/premier-league.png';
-import { Box } from '@mui/material';
-import { getMatches } from '../Services';
+import React, { useEffect, useState } from "react";
+import {
+  ArrangeTable,
+  MatchesTable,
+  PageBanner,
+  RangeTeamsGoals,
+  TeamStatistics,
+} from "../Components";
+import PremierLeague from "../assets/images/premier-league.png";
+import { Box } from "@mui/material";
+import { getMatches } from "../Services";
+import { useLocation } from "react-router-dom";
 
 const Leagues = () => {
+  const { pathname } = useLocation();
   const urlParams = new URLSearchParams(window.location.search);
-  const seasonId = urlParams.get('season_id');
+  const seasonId = urlParams.get("season_id");
 
   const [matches, setMatches] = useState([]);
+  const [path, setPath] = useState([]);
 
-  
   useEffect(() => {
+    const path = pathname.split("/")[2];
+    setPath(path);
+    console.log("path: ", path);
     const fetchMatches = async () => {
-      const response = await getMatches('season_id=' + seasonId);
+      const response = await getMatches("season_id=" + seasonId);
       setMatches(response?.data?.data);
+      console.log("response?.data?.data: ", response?.data?.data);
     };
     fetchMatches();
   }, [seasonId]);
 
-
   return (
     <>
-      <Box flex='3'>
+      <Box flex="3">
         <main>
           <PageBanner
-            leagueImage={PremierLeague}
-            leagueName={'الدوري الممتاز'}
+            leagueImage={`https://cdn.so3ody.com/scores/competitions/100x130/${path}.png`}
+            leagueName={""}
           />
           <MatchesTable matches={matches} />
           <ArrangeTable />
           <TeamStatistics />
         </main>
       </Box>
-      <Box flex='1'>
+      <Box flex="1">
         <RangeTeamsGoals />
       </Box>
     </>
